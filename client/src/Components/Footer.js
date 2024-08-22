50d711ba37b0b5c7fbec3e060cd26af55e142697
Muhammad Abubakar Jamiu <muhammadabubakarjamiu@gmail.com>
2024-08-22 11:37:54 -0700

    add navbar

---
+++ client/src/Components/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Footer.css"
+
+function Footer() {
+  return (
+    
+        <footer>
+       <div class="row copyright">
+  <div class="footer-menu">
+  <a href="">About</a>
+  <a href="">Terms Of Service</a>
+
+  </div>
+   <p>Copyright. All rights Reserved &copy; 2024</p>
+   <p>made with &hearts; love &hearts;  by Accidic</p>
+</div>
+
+        </footer>
+
+  )
+}
+
+export default Footer
